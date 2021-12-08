690d39ebab86bcc536e2347ce922f6496f24497d
Tuan Vu <tuanvd@gmail.com>
2021-12-08 20:42:59 +0700

    Update code that requires winner claiming jackpot instead of sending automatically

--- CoinFlipJackpot.sol
+++ CoinFlipJackpot.sol
@@ -55,17 +55,35 @@ interface IArenaPlayerStorage {
         ) external;
 }
 
-// TODO: update code that requires winner claiming the jackpot instead of sending automatically as current !
+
 contract CoinFlipJackpot is IERC20Receiver, ICoinFlipGameExtension, Ownable, TransferableFund(new address[](0)){
     ICoinFlipGameHouse public immutable gameHouse;
     uint256 public jackpotAmt;              // jackpot accumulated amount
+    
     uint256 public accumulatedFromTime;     // the time when the jackpot begun to accummulate
-    uint256 public immutable chanceToWin;   // chanceToWin = 1000 means probability of jackpot trigger is 0.1%
+    
+    // chanceToWin = 1000 means probability of jackpot trigger is 0.1%
+    // Note that this param is set once and cannot be modified later to avoiding fraudulence of game house
+    // (e.g. the game house owner reduces `chanceToWin` to 1 then plays the game to loot the jackpot, 
+    //  then sets it back to normal value)
+    uint256 public immutable chanceToWin;
+    
+    // unclaimed won jackpot(s) of player
+    struct WonJackpot {
+        uint256 betId;
+        uint256 amount;
+        uint256 time;
+    }
+    mapping(address => WonJackpot[]) public unclaimedWonJackpots;
+    
+    uint256 public totalUnclaimedWonJackpotAmt;
+    
     address public lastWinner;
     uint256 public lastWonAmt;
     uint256 public lastWonTime;
     
     event Triggered(address indexed player, uint256 amount, uint256 indexed betId, uint256 time);
+    event ClaimJackpot(address indexed player, uint256 amount, uint256 indexed betId, uint256 time);
     event Withdraw(address indexed sender, address to, uint256 amount, uint256 time);
     
     
@@ -115,13 +133,22 @@ contract CoinFlipJackpot is IERC20Receiver, ICoinFlipGameExtension, Ownable, Tra
         // do the roll
         if((randomNum % chanceToWin) == 0){
             // player wins the jackpot!
-            uint256 transferedAmt = _safeTokenTransfer(address(gameHouse.paymentCurrency()), jackpotAmt, player);
-            emit Triggered(player, transferedAmt, _betId, block.timestamp);
+            //uint256 transferedAmt = _safeTokenTransfer(address(gameHouse.paymentCurrency()), jackpotAmt, player);
+            unclaimedWonJackpots[player].push(WonJackpot({
+                betId: _betId,
+                amount: jackpotAmt,
+                time:  block.timestamp
+            }));
+            totalUnclaimedWonJackpotAmt += jackpotAmt;
+            
+            emit Triggered(player, jackpotAmt, _betId, block.timestamp);
+            
             jackpotAmt = 0;
             accumulatedFromTime = 0;
             lastWinner = player;
-            lastWonAmt = transferedAmt;
+            lastWonAmt = jackpotAmt;
             lastWonTime = block.timestamp;
+            
             // update player storage
             gameHouse.playerStorage().updateGameData(address(0), player, false, true, jackpotAmt, 0);
         }
@@ -135,12 +162,33 @@ contract CoinFlipJackpot is IERC20Receiver, ICoinFlipGameExtension, Ownable, Tra
         
     }
     
+    function totalUnclaimedJackpots() external view returns(uint256){
+        return unclaimedWonJackpots[msg.sender].length;
+    }
+    
+    function claimJackpot(uint256 _index) external {
+        WonJackpot[] storage playerUnclaimedJackpots = unclaimedWonJackpots[msg.sender];
+        uint256 total = playerUnclaimedJackpots.length;
+        
+        require(total > 0, "No jackpot to claim");
+        require(_index < total, "Bad input");
+        
+        uint256 wonAmount = playerUnclaimedJackpots[_index].amount;
+        uint256 transferedAmt = _safeTokenTransfer(address(gameHouse.paymentCurrency()), wonAmount, msg.sender);
+        totalUnclaimedWonJackpotAmt -= transferedAmt;
+        emit ClaimJackpot(msg.sender, wonAmount, playerUnclaimedJackpots[_index].betId, block.timestamp);
+        delete playerUnclaimedJackpots[_index];
+    }
+    
     // NOTE: have this function or not?
     function withdraw(address _to) external onlyOwner {
-        uint256 withdrawalAmt = _safeTokenTransfer(address(gameHouse.paymentCurrency()), jackpotAmt, _to);
-        emit Withdraw(msg.sender, _to, withdrawalAmt, block.timestamp);
-        jackpotAmt = 0;
-        accumulatedFromTime = 0;
+        uint256 withdrawalAmt = jackpotAmt > totalUnclaimedWonJackpotAmt ? jackpotAmt-totalUnclaimedWonJackpotAmt : 0;
+        if(withdrawalAmt > 0){
+            withdrawalAmt = _safeTokenTransfer(address(gameHouse.paymentCurrency()), withdrawalAmt, _to);
+            emit Withdraw(msg.sender, _to, withdrawalAmt, block.timestamp);
+            jackpotAmt = 0;
+            accumulatedFromTime = 0;
+        }
     }
     
 }
